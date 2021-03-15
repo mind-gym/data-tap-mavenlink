d5addd559fb5694995994441c3178e7200b8585a
Adrien Czerny <adrien.czerny@themindgym.com>
2021-03-15 17:25:16 +0000

    Added streams: custom_field_sets (http://developer.mavenlink.com/custom_field_sets/), and custom_field_values streams for subjects: WorkspaceGroup, Story, User (http://developer.mavenlink.com/custom_field_values/)

--- tap_mavenlink/streams/__init__.py
+++ tap_mavenlink/streams/__init__.py
@@ -1,6 +1,7 @@
 from tap_mavenlink.streams.account_memberships import AccountMembershipsStream
 from tap_mavenlink.streams.assignments import AssignmentsStream
 from tap_mavenlink.streams.custom_fields import CustomFieldsStream
+from tap_mavenlink.streams.custom_field_sets import CustomFieldSetsStream
 from tap_mavenlink.streams.workspace_custom_field_values import WorkspaceCustomFieldValuesStream
 from tap_mavenlink.streams.estimates import EstimatesStream
 from tap_mavenlink.streams.estimates_scenarios import EstimatesScenariosStream
@@ -10,17 +11,20 @@ from tap_mavenlink.streams.expenses import ExpensesStream
 from tap_mavenlink.streams.invoices import InvoicesStream
 from tap_mavenlink.streams.posts import PostsStream
 from tap_mavenlink.streams.stories import StoriesStream
+from tap_mavenlink.streams.story_custom_field_values import StoryCustomFieldValuesStream
 from tap_mavenlink.streams.time_entries import TimeEntriesStream
 from tap_mavenlink.streams.users import UsersStream
+from tap_mavenlink.streams.user_custom_field_values import UserCustomFieldValuesStream
 from tap_mavenlink.streams.workspace_allocations import WorkspaceAllocationsStream
 from tap_mavenlink.streams.workspace_groups import WorkspaceGroupsStream
+from tap_mavenlink.streams.workspace_group_custom_field_values import WorkspaceGroupCustomFieldValuesStream
 from tap_mavenlink.streams.workspaces import WorkspacesStream
 
 AVAILABLE_STREAMS = [
     AccountMembershipsStream,
     AssignmentsStream,
     CustomFieldsStream,
-    WorkspaceCustomFieldValuesStream,
+    CustomFieldSetsStream,
 
     # Important: sync EstimatesScenarios before EstimatesResources
     EstimatesStream,
@@ -37,6 +41,12 @@ AVAILABLE_STREAMS = [
     WorkspaceAllocationsStream,
     WorkspaceGroupsStream,
     WorkspacesStream,
+
+    # Custom field values streams
+    WorkspaceCustomFieldValuesStream,
+    WorkspaceGroupCustomFieldValuesStream,
+    StoryCustomFieldValuesStream,
+    UserCustomFieldValuesStream,
 ]
 
 

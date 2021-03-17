309d2226c6d466b12cc0e793aad920e39dd4a053
Adrien Czerny <adrien.czerny@themindgym.com>
2021-03-17 10:30:16 +0000

    Added expected discovery json file to tests for regression testing

--- tests/test_discover.py
+++ tests/test_discover.py
@@ -5,11 +5,25 @@ from tests import constants
 from tap_mavenlink import main
 
 
-def test_discover_no_catalog(tmp_path, config):
+EXPECTED_DISCOVERY_CONTENT = './tests/discovery.json'
+
+
+def test_discover_no_catalog(tmp_path, config, capsys):
+    # Given
     sys_args = [
         'tap-mavenlink',
         '--config', str(tmp_path / constants.CONFIG_FILENAME),
         '--discover'
     ]
+
+    # When
     with patch('sys.argv', sys_args):
         main()
+
+    captur = capsys.readouterr()
+    stdout = captur.out
+    with open(EXPECTED_DISCOVERY_CONTENT, 'r') as f:
+        expected_discovery = f.read()
+
+    # Then
+    assert stdout == expected_discovery

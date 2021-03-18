c5e0e996742d84436f3d6ba0842e5356ccfbf9fa
Adrien Czerny <adrien.czerny@themindgym.com>
2021-03-18 17:47:00 +0000

    Renamed `discovery` variable names to something more like `catalog`, fixed the tests to match `dict` objects instead of `str` objects.

--- tests/test_discover.py
+++ tests/test_discover.py
@@ -7,8 +7,8 @@ from tests import constants
 from tap_mavenlink import main
 
 
-EXPECTED_DISCOVERY_CONTENT = './tests/discovery.json'
-EXPECTED_DISCOVERY_SCHEMA_CONTENT = './tests/catalog-schema.json'
+EXPECTED_CATALOG_DISCOVERED_CONTENT = './tests/catalog-discovered.json'
+EXPECTED_CATALOG_SCHEMA_CONTENT = './tests/catalog-schema.json'
 
 
 def test_discover_no_catalog(tmp_path, config, capsys):
@@ -24,14 +24,14 @@ def test_discover_no_catalog(tmp_path, config, capsys):
         main()
 
     captur = capsys.readouterr()
-    stdout = captur.out
+    catalog_discovered = json.loads(captur.out)
 
-    with open(EXPECTED_DISCOVERY_CONTENT, 'r') as f:
-        expected_discovery = f.read()
+    with open(EXPECTED_CATALOG_DISCOVERED_CONTENT, 'r') as f:
+        expected_catalog_discovered = json.loads(f.read())
 
-    with open(EXPECTED_DISCOVERY_SCHEMA_CONTENT, 'r') as f:
-        expected_discovery_schema = f.read()
+    with open(EXPECTED_CATALOG_SCHEMA_CONTENT, 'r') as f:
+        expected_catalog_schema = json.loads(f.read())
 
     # Then
-    assert stdout == expected_discovery
-    jsonschema.validate(json.loads(expected_discovery), json.loads(expected_discovery_schema))
+    assert catalog_discovered == expected_catalog_discovered
+    jsonschema.validate(expected_catalog_discovered, expected_catalog_schema)

489a6b4d6847c378dff1527ab6a78cdafbf1c5ef
Adrien Czerny <adrien.czerny@themindgym.com>
2021-03-22 11:41:55 +0000

    Making custom_field_values schemas consistent for subject user, story along with already existing workspace. The `value` property is now considered of type `string` even though there are records in Mavenlink where values are arrays of integers. We decide we will exclude them from replication in any further target process.

--- tests/test_catalog_discovered.py
+++ tests/test_catalog_discovered.py
@@ -7,11 +7,10 @@ from tests import constants
 from tap_mavenlink import main
 
 
-EXPECTED_CATALOG_DISCOVERED_CONTENT = './tests/catalog-discovered.json'
 EXPECTED_CATALOG_SCHEMA_CONTENT = './tests/catalog-schema.json'
 
 
-def test_discover_no_catalog(tmp_path, config, capsys):
+def test_catalog_discovered_schema_correct(tmp_path, config, capsys):
     # Given
     sys_args = [
         'tap-mavenlink',
@@ -26,12 +25,8 @@ def test_discover_no_catalog(tmp_path, config, capsys):
     captur = capsys.readouterr()
     catalog_discovered = json.loads(captur.out)
 
-    with open(EXPECTED_CATALOG_DISCOVERED_CONTENT, 'r') as f:
-        expected_catalog_discovered = json.loads(f.read())
-
     with open(EXPECTED_CATALOG_SCHEMA_CONTENT, 'r') as f:
         expected_catalog_schema = json.loads(f.read())
 
     # Then
-    assert catalog_discovered == expected_catalog_discovered
-    jsonschema.validate(expected_catalog_discovered, expected_catalog_schema)
+    jsonschema.validate(catalog_discovered, expected_catalog_schema)

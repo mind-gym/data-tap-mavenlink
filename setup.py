a338675726ed28df3f1c0442b78d791b9dad5d2e
graham.jackson <graham.jackson@themindgym.com>
2021-03-23 10:25:17 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='data-tap-mavenlink',
-      version='0.2.1',
+      version='0.2.2',
       description='Singer.io tap for extracting data from the Mavenlink API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',

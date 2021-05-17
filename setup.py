dcd707a746b7a20fda1f2b7e2ff7c6585e4f9cf3
Adrien Czerny <adrien.czerny@themindgym.com>
2021-05-17 11:56:37 +0100

    Bumped minor version to 0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='data-tap-mavenlink',
-      version='0.2.2',
+      version='0.2.3',
       description='Singer.io tap for extracting data from the Mavenlink API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
